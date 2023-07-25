bc44086b8410ddb0b12856c80b056354540f9f0c
ramcharan16 <109079783+ramcharan16@users.noreply.github.com>
2023-07-25 18:23:17 +0530

    dom for html

---
+++ .js
@@ -0,0 +1,28 @@
+//EXAMINE THE DOCUMENT OBJECT//
+
+//console.dir(document);
+//console.log(document.domain);
+//console.log(document.URL);
+//console.log(document.title);
+//console.log(document.doctype);
+//console.log(document.head);
+//console.log(document.body);
+//console.log(document.all[10]);
+//console.log(document.forms);
+//console.log(document.links);
+//console.log(document.images);
+
+//getElementById;
+//console.log(document.getElementById('header-title'));
+var headerTitle=document.getElementById('header-title');
+var header=document.getElementById('main-header');
+//console.log(headerTitle);
+//headerTitle.textContent='Hello';
+//headerTitle.innerText='Goodbye';
+//console.log(headerTitle.textContent);
+//headerTitle.innerHTML='<h3>Hello</h3>';
+var titles=document.getElementById('main');
+titles.style.color='green';
+titles.style.fontWeight='bold';
+
+
